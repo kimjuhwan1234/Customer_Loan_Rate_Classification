5f333d487f7304de1c458b501ddee077b3b4c6a4
Kim Juhwan <asas4539@hanyang.ac.kr>
2024-01-19 15:47:44 +0900

    .

--- Codes/1_main.py
+++ Codes/1_main.py
@@ -1,12 +1,13 @@
 from xgboost import XGBClassifier
 from catboost import CatBoostClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 
 import warnings
 import pandas as pd
+import lightgbm as lgb
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
@@ -15,7 +16,7 @@ if __name__ == "__main__":
     y = train['대출등급']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    RF = True
+    RF = False
     if RF:
         rf_model = RandomForestClassifier()
         param_grid = {
@@ -33,17 +34,40 @@ if __name__ == "__main__":
         # rf_model.fit(X_train, y_train)
         # print("Random Forest Accuracy:", accuracy_score(y_test, rf_model.predict(X_test)))
 
-    GB = False
-    if GB:
-        gb_model = GradientBoostingClassifier(n_estimators=10, random_state=42)
-        gb_model.fit(X_train, y_train)
-        print("Gradient Boosting Accuracy:", accuracy_score(y_test, gb_model.predict(X_test)))
+    GBM = True
+    if GBM:
+        params = {
+            'objective': 'multiclass',
+            'metric': 'multi_logloss',
+            'num_class': 7,
+            'device': 'cpu',
+            'gpu_platform_id': 0,
+            'gpu_device_id': 0
+        }
+
+        train_data = lgb.Dataset(X_train, label=y_train)
+        valid_data = lgb.Dataset(X_test, label=y_test, reference=train_data)
+
+        num_round = 100
+        bst = lgb.train(params, train_data, num_round, valid_sets=[valid_data])
+
+        y_pred = bst.predict(X_test, num_iteration=bst.best_iteration)
+        y_pred_class = [int(pred.argmax()) for pred in y_pred]
+
+        accuracy = accuracy_score(y_test, y_pred_class)
+        classification_rep = classification_report(y_test, y_pred_class)
+
+        print(f"Accuracy: {accuracy}")
+        print("Classification Report:\n", classification_rep)
 
     XGB = False
     if XGB:
-        xgb_model = XGBClassifier(n_estimators=1000, random_state=2024, learning_rate=0.01, max_depth=10)
+        params = {'tree_method': 'gpu_hist', 'gpu_id': 0}
+        xgb_model = XGBClassifier(n_estimators=1000, random_state=2024, learning_rate=0.01, max_depth=10, **params)
+
         xgb_model.fit(X_train, y_train)
         print("XGBoost Accuracy:", accuracy_score(y_test, xgb_model.predict(X_test)))
+
 
     Cat = False
     if Cat:
@@ -52,7 +76,8 @@ if __name__ == "__main__":
                                        learning_rate=0.01,
                                        depth=10,
                                        l2_leaf_reg=3,
-                                       metric_period=1000)
+                                       metric_period=1000,
+                                       task_type='GPU')
         cat_model.fit(X_train, y_train)
         print("CatBoost Accuracy:", accuracy_score(y_test, cat_model.predict(X_test)))
 
@@ -60,10 +85,9 @@ if __name__ == "__main__":
     if VOTE:
         voting_clf = VotingClassifier(estimators=[
             ('random_forest', rf_model),
-            ('gradient_boosting', gb_model),
             ('XGB_Boosting', xgb_model),
             ('CatBoosting', cat_model)
-        ], voting='soft')
+            ], voting='soft')
 
         voting_clf.fit(X_train, y_train)
         print("Voting Classifier Accuracy:", accuracy_score(y_test, voting_clf.predict(X_test)))
