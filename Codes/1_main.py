17c5574eb25a8eaa607c78b834ff7fafe48192fd
Kim Juhwan <asas4539@hanyang.ac.kr>
2024-01-19 20:33:50 +0900

    .

--- Codes/1_main.py
+++ Codes/1_main.py
@@ -1,11 +1,11 @@
 from xgboost import XGBClassifier
 from catboost import CatBoostClassifier
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 
 import warnings
+import numpy as np
 import pandas as pd
 import lightgbm as lgb
 
@@ -16,25 +16,35 @@ if __name__ == "__main__":
     y = train['대출등급']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    RF = False
+    RF = True
     if RF:
-        rf_model = RandomForestClassifier()
-        param_grid = {
-            'n_estimators': [10, 50, 100],
-            'max_depth': [None, 10, 20],
-            'min_samples_split': [2, 5, 10],
-            'min_samples_leaf': [1, 2, 4],
-            'max_features': ['auto', 'sqrt', 'log2']
+        param = {
+            'n_estimators': 500,
+            'criterion': 'gini',
+            'max_depth': 50,
+            'min_samples_split': 2,
+            'min_samples_leaf': 1,
+            'class_weight': {0: 8, 1: 5, 2: 2, 3: 1, 4: 1 / 3, 5: 1 / 3, 6: 1 / 3},
+            'oob_score': True,
+
         }
-        grid_search = GridSearchCV(estimator=rf_model, param_grid=param_grid, cv=3, scoring='accuracy')
-        grid_search.fit(X_train, y_train)
-        print("Best Hyperparameters:", grid_search.best_params_)
+        # {0: 8, 1: 5, 2: 2, 3: 1, 4: 1/3, 5: 1/3, 6: 1/3}
+        rf_model = RandomForestClassifier(**param)
+        rf_model.fit(X_train, y_train)
+        unique_classes = np.unique(y_train)
+        class_oob_scores = {}
 
-        # rf_model = RandomForestClassifier(n_estimators=10, max_depth=None, random_state=42)
-        # rf_model.fit(X_train, y_train)
-        # print("Random Forest Accuracy:", accuracy_score(y_test, rf_model.predict(X_test)))
+        for class_label in unique_classes:
+            class_indices = (y_train == class_label)
+            class_oob_score = accuracy_score(y_train[class_indices], rf_model.predict(X_train[class_indices]))
+            class_oob_scores[class_label] = class_oob_score
 
-    GBM = True
+        print("Class-wise OOB Scores:", class_oob_scores)
+
+        print("Random Forest Accuracy:", accuracy_score(y_test, rf_model.predict(X_test)))
+        print("OOB Score:", rf_model.oob_score)
+
+    GBM = False
     if GBM:
         params = {
             'objective': 'multiclass',
@@ -68,7 +78,6 @@ if __name__ == "__main__":
         xgb_model.fit(X_train, y_train)
         print("XGBoost Accuracy:", accuracy_score(y_test, xgb_model.predict(X_test)))
 
-
     Cat = False
     if Cat:
         cat_model = CatBoostClassifier(random_state=2024,
@@ -87,7 +96,7 @@ if __name__ == "__main__":
             ('random_forest', rf_model),
             ('XGB_Boosting', xgb_model),
             ('CatBoosting', cat_model)
-            ], voting='soft')
+        ], voting='soft')
 
         voting_clf.fit(X_train, y_train)
         print("Voting Classifier Accuracy:", accuracy_score(y_test, voting_clf.predict(X_test)))
