28b399184ce950d7f9cb912078f527ae92bc5eb0
Kim Juhwan <asas4539@hanyang.ac.kr>
2024-01-28 16:03:07 +0900

    .

--- Codes/1_main.py
+++ Codes/1_main.py
@@ -1,136 +1,111 @@
-from xgboost import XGBClassifier
-from catboost import CatBoostClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 # from pytorch_tabnet.multitask import TabNetMultiTaskClassifier
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, VotingClassifier
-from lightgbm import LGBMClassifier
-from sklearn.preprocessing import LabelEncoder
 
 # import torch
 import joblib
 import warnings
 import numpy as np
 import pandas as pd
+import xgboost as xgb
 import lightgbm as lgb
+
+
 # import torch.nn.functional as F
+
+def AdaBoost(params, X_train, y_train, X_test, y_test):
+    ada_model = AdaBoostClassifier(**params)
+    ada_model.fit(X_train, y_train)
+    print("AdaBoost Accuracy:", accuracy_score(y_test, ada_model.predict(X_test)))
+
+
+def lightGBM(params, X_train, y_train, X_test, y_test, num_rounds):
+    train_data = lgb.Dataset(X_train, label=y_train)
+    valid_data = lgb.Dataset(X_test, label=y_test, reference=train_data)
+
+    bst = lgb.train(params, train_data, num_rounds, valid_sets=[valid_data])
+    y_pred = bst.predict(X_test, num_iteration=bst.best_iteration)
+    predictions = [int(pred.argmax()) for pred in y_pred]
+
+    joblib.dump(bst, '../Files/gbm_model.pkl')
+    print("lightGBM Accuracy:", accuracy_score(y_test, predictions))
+
+    return y_pred
+
+
+def XGBoost(params, X_train, y_train, X_test, y_test, num_rounds):
+    train_data = xgb.DMatrix(X_train, label=y_train)
+    valid_data = xgb.DMatrix(X_test, label=y_test)
+
+    bst = xgb.train(params, train_data, num_rounds, evals=[(valid_data, 'eval')])
+    X_test = xgb.DMatrix(X_test)
+    y_pred = bst.predict(X_test)
+    predictions = y_pred.argmax(axis=1)
+
+    joblib.dump(bst, '../Files/xgb_model.pkl')
+    print("XGBoost Accuracy:", accuracy_score(y_test, predictions))
+
+    return y_pred
+
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
-    original= pd.read_csv('../Database/train.csv')
-    train = pd.read_csv('../Database/train_modified4.csv', index_col='ID')
-    test = pd.read_csv('../Database/test_modified4.csv', index_col='ID')
+    original = pd.read_csv('../Database/train.csv')
+    train = pd.read_csv('../Database/train_preprocessed.csv', index_col='ID')
     X = train.drop(columns=['대출등급'])
     y = train['대출등급']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    RF = False
-    if RF:
-        params = {
-            'n_estimators': 100,
-            'criterion': 'entropy',
-            'max_depth': 80,
-            'min_samples_split': 2,
-            'min_samples_leaf': 2,
-            'class_weight': {0: 8, 1: 5, 2: 2, 3: 1, 4: 1 / 3, 5: 1 / 3, 6: 1 / 3},
-        }
-        # {0: 8, 1: 5, 2: 2, 3: 1, 4: 1/3, 5: 1/3, 6: 1/3}
-        rf_model = RandomForestClassifier(**params)
-        rf_model.fit(X_train, y_train)
-        print("Random Forest Accuracy:", accuracy_score(y_test, rf_model.predict(X_test)))
-
-    Ada = False
+    Ada = True
     if Ada:
         params = {
-            'base_estimator': DecisionTreeClassifier(max_depth=10),
+            'base_estimator': DecisionTreeClassifier(max_depth=5),
             'n_estimators': 100,
             'learning_rate': 1,
             'algorithm': 'SAMME.R',
             'random_state': 42
         }
 
-        ada_model = AdaBoostClassifier(**params)
-        ada_model.fit(X_train, y_train)
-        print("AdaBoost Accuracy:", accuracy_score(y_test, ada_model.predict(X_test)))
+        AdaBoost(params, X_train, y_train, X_test, y_test)
 
     GBM = False
     if GBM:
         params = {
-            'learning_rate': 0.05,
+            'device': 'cpu',
             'boosting_type': 'gbrt',
             'objective': 'multiclass',
             'metric': 'multi_logloss',
+
             'num_class': 7,
-            'device': 'cpu',
+            'learning_rate': 0.05,
+            'max_depth': 15,
+
             'num_leaves': 100,
-            'max_depth': 20,
             'min_data_in_leaf': 2,
             'tree_learner': 'voting',
         }
 
-        train_data = lgb.Dataset(X_train, label=y_train)
-        valid_data = lgb.Dataset(X_test, label=y_test, reference=train_data)
-
-        num_round = 100
-        bst = lgb.train(params, train_data, num_round, valid_sets=[valid_data])
-        y_pred = bst.predict(X_test, num_iteration=bst.best_iteration)
-        y_pred_class = [int(pred.argmax()) for pred in y_pred]
-
-        joblib.dump(bst, '../Files/gbm_model.pkl')
-        print("lightGBM Accuracy:", accuracy_score(y_test, y_pred_class))
+        proba1 = lightGBM(params, X_train, y_train, X_test, y_test, 200)
 
     XGB = False
     if XGB:
-        params = {'tree_method': 'gpu_hist', 'gpu_id': 0}
-        xgb_model = XGBClassifier(n_estimators=1000, random_state=2024, learning_rate=0.01, max_depth=20, **params)
+        params = {
+            'device': 'cuda',
+            'booster': 'gbtree',
+            'objective': 'multi:softprob',
+            'eval_metric': 'mlogloss',
 
-        xgb_model.fit(X_train, y_train)
+            'num_class': 7,
+            'eta': 0.05,
+            'max_depth': 15,
 
-        joblib.dump(xgb_model, '../Files/xgb_model.pkl')
-        print("XGBoost Accuracy:", accuracy_score(y_test, xgb_model.predict(X_test)))
+            'gamma': 3,
+            'subsample': 0.6,
+        }
 
-    VOTE = True
-    if VOTE:
-        xgb_model = joblib.load('../Files/xgb_model.pkl')
-        cat_model = joblib.load('../Files/cat_model.pkl')
-        gbm_model = joblib.load('../Files/gbm_model.pkl')
-
-        proba1 = xgb_model.predict_proba(X_test)
-        # proba2 = cat_model.predict_proba(X_test)
-        proba3 = gbm_model.predict(X_test, num_iteration=gbm_model.best_iteration)
-
-        xgb_result = np.argmax(proba1, axis=1)
-        # cat_result = np.argmax(proba2, axis=1)
-        gbm_result = [int(pred.argmax()) for pred in proba3]
-
-        print("XGBoost Accuracy:", accuracy_score(y_test, xgb_result))
-        # print("CatBoost Accuracy:", accuracy_score(y_test, cat_result))
-        print("lightGBM Accuracy:", accuracy_score(y_test, gbm_result))
-
-        average_proba = (proba1 + proba3) / 2
-        soft_voting_result = np.argmax(average_proba, axis=1)
-        print("Soft Voting Accuracy:", accuracy_score(y_test, soft_voting_result))
-
-        if True:
-            proba1 = xgb_model.predict_proba(test)
-            # proba2 = cat_model.predict_proba(test)
-            proba3 = gbm_model.predict(test, num_iteration=gbm_model.best_iteration)
-            average_proba = (proba1+proba3)/2
-            soft_voting_result = np.argmax(average_proba, axis=1)
-
-            answer = pd.read_csv('../Database/sample_submission.csv')
-
-            label_encoder = LabelEncoder()
-            encoded_data = label_encoder.fit_transform(original['대출등급'])
-            decoded_data = label_encoder.inverse_transform(soft_voting_result)
-
-            # 복원된 데이터를 DataFrame으로 변환
-            df_restored = pd.DataFrame({'대출등급': decoded_data})
-
-            answer['대출등급']=decoded_data
-
-            answer.to_csv('../Files/answer.csv', index=None)
+        proba2 = XGBoost(params, X_train, y_train, X_test, y_test, 200)
 
 
     Tab = False
