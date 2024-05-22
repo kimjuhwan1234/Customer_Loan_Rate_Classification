e5a717a38f37974c56fcb81de6306da0f2a670d8
Kim Juhwan <asas4539@hanyang.ac.kr>
2024-05-22 18:16:27 +0900

    .

--- main.py
+++ main.py
@@ -11,24 +11,24 @@ if __name__ == "__main__":
     X_val = pd.read_csv('Database/X_val.csv', index_col=0)
     y_val = pd.read_csv('Database/y_val.csv', index_col=0)
 
-    sampling_name = ['SMOTE', 'ADASYN', 'KMeanSMOTE']
-    for name in enumerate(sampling_name):
-        if name == 'SMOTE':
-            smote = SMOTE(sampling_strategy='minority', random_state=42)
+    sampling_name = ['SMOTE_all', 'KMeanSMOTE_all']
+    for i, name in enumerate(sampling_name):
+        if name == 'SMOTE_all':
+            smote = SMOTE(sampling_strategy='all', random_state=42)
             X_train, y_train = smote.fit_resample(X_train, y_train)
 
         elif name == 'ADASYN':
             adasyn = ADASYN(sampling_strategy='minority', random_state=42)
             X_train, y_train = adasyn.fit_resample(X_train, y_train)
 
-        elif name == 'KMeanSMOTE':
-            kmsmote = KMeansSMOTE(sampling_strategy='minority', random_state=42, kmeans_estimator=7,
+        elif name == 'KMeanSMOTE_all':
+            kmsmote = KMeansSMOTE(sampling_strategy='all', random_state=42, kmeans_estimator=7,
                                   cluster_balance_threshold=0.01)
             X_train, y_train = kmsmote.fit_resample(X_train, y_train)
 
         for i in range(4):
             # {DT=0, lightGBM=1, XGBoost=2, CatBoost=3}
-            E = Esemble(i, X_train, X_val, y_train, y_val, 1000, sampling_name[j])
+            E = Esemble(i, X_train, X_val, y_train, y_val, 1000, name)
 
             study = optuna.create_study(direction='maximize')
             study.optimize(E.objective, n_trials=100)
